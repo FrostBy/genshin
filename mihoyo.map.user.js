1a42db9b92f14e21402ef12fd821e47d13da5e02
Vladislav Romanovsky <frost.by@gmail.com>
2022-03-30 23:13:45 +0300

    v2.6 map update

--- mihoyo.map.user.js
+++ mihoyo.map.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Genshin Map Auto
 // @namespace    http://mihoyo.com/
-// @version      0.1
+// @version      0.2
 // @description  Toggle Markers without with single right click!
 // @author       Vladislav Romanovsky
-// @match        https://webstatic-sea.mihoyo.com/app/ys-map-sea/*
-// @icon         https://www.google.com/s2/favicons?domain=webstatic-sea.mihoyo.com/app/ys-map-sea
+// @match        https://webstatic-sea.hoyolab.com/ys/app/interactive-map/index.html*
+// @icon         https://webstatic-sea.hoyolab.com/ys/app/interactive-map/mapicon.png
 // @updateURL    https://github.com/FrostBy/genshin/raw/master/mihoyo.map.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
 // ==/UserScript==
@@ -22,11 +22,18 @@
         return this.each(function () { this.dispatchEvent(clickEvent); });
     };
 
-    $(document).on('mousedown', '.mhy-game-gis-marker', function (event) {
-        if (event.which === 3) {
-            $(event.target).triggerRawMouse();
-            $('.map-popup__switch').triggerRawMouse();
-            $('.leaflet-popup-close-button').triggerRawMouse();
+    const interval = setInterval(() => {
+        if ($('.mhy-game-gis')[0]?.__vue__?.map) {
+            clearInterval(interval);
+            $('.mhy-game-gis')[0].__vue__.map.on('mousedown', function (event) {
+                console.log(event);
+                if (event?.originalEvent?.which === 3) {
+                    event.type = 'click';
+                    event.target._events.click[0].fn.apply(event.target.ciLayer, [event]);
+                    $('.map-popup__switch').triggerRawMouse();
+                    $('.leaflet-popup-close-button').triggerRawMouse();
+                }
+            });
         }
-    });
+    }, 1000);
 })();
